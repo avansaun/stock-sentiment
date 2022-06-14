17d826294dee9cf526c4fb9f931f3e86921dd1be
Andrew Van Saun <andrew@andrews-macbook.lan>
2022-06-13 20:38:33 -0700

    Improvements to sentiment graph

--- request.py
+++ request.py
@@ -1,9 +1,9 @@
-from typing import final
 import requests
 import matplotlib.pyplot as plt
 import json
+import dateutil.parser
 
-#Gets sentiment data from GDELT and returns it as JSON data
+#----Gets sentiment data from GDELT and returns it as JSON data----
 searchPhrase = input("Stock Ticker Symbol or Company Name: ")
 
 baseUrl = "https://api.gdeltproject.org/api/v2/doc/doc?"
@@ -13,18 +13,29 @@ finalUrl = baseUrl + query + "&mode=TimelineTone&format=JSON"
 req = requests.get(url = finalUrl)
 
 response = req.json()
-print(response)
 
-#plot the data
+#----Load test data from file----
+# testFile = open("TestData/tslastock-sentiment-timeline.json")
+# testData = json.load(testFile)
+
 dataPoints = response['timeline'][0]['data']
-xAxis = [value for key, value in dataPoints] 
-#yAxis = [value for key, value in dataPoints['value']]
-#print(xAxis)
 
-# plt.grid(True)
+#----Create Graph Axes----
+xAxisValues = []
+for date in dataPoints:
+    dateStr = dateutil.parser.parse(date['date'])
+    readableDate = dateStr.strftime('%m/%d/%Y')
+    xAxisValues.append(readableDate)
 
-# plt.plot(xAxis, yAxis, color='blue', marker='o')
-# plt.xlabel('Date')
-# plt.ylabel('Average Tone')
+yAxisValues = [item['value'] for item in dataPoints]
+xAxisTickLabels = xAxisValues[::7] # get every 7th value of the array, so not every tick has a label
 
-# plt.show()
+plt.grid(True)
+plt.plot(xAxisValues, yAxisValues, color='blue', marker='o')
+plt.xlabel('Date')
+plt.ylabel('Average Tone')
+plt.xticks(rotation = 90)
+axes = plt.gca()
+axes.get_xaxis().set_ticks(xAxisTickLabels)
+
+plt.show()
