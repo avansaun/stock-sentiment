8d5b3d53e3ae9cd32258fda31459a6d71c6d3523
Andrew Van Saun <andrew@andrews-macbook.lan>
2022-06-13 21:26:14 -0700

    Adding initial GUI

---
+++ SentimentData.py
@@ -0,0 +1,52 @@
+from matplotlib.markers import MarkerStyle
+import requests
+import matplotlib.pyplot as plt
+import json
+import dateutil.parser
+
+class SentimentData():
+
+    #----Get sentiment data from GDELT----
+    def QuerySentimentData():
+        searchPhrase = input("Stock Ticker Symbol or Company Name: ")
+
+        baseUrl = "https://api.gdeltproject.org/api/v2/doc/doc?"
+        query = "query=%22" + searchPhrase + " %20stock" + "%22"
+        finalUrl = baseUrl + query + "&mode=TimelineTone&format=JSON"
+
+        req = requests.get(url = finalUrl)
+
+        response = req.json()
+
+        #----Load test data from file----
+        # testFile = open("TestData/tslastock-sentiment-timeline.json")
+        # testData = json.load(testFile)
+
+        dataPoints = response['timeline'][0]['data']
+        return dataPoints
+
+
+    #----Graph The Sentiment Data----
+    def GraphDataValues(dataPoints):
+        xAxisValues = []
+        for date in dataPoints:
+            dateStr = dateutil.parser.parse(date['date'])
+            readableDate = dateStr.strftime('%m/%d/%Y')
+            xAxisValues.append(readableDate)
+
+        yAxisValues = [item['value'] for item in dataPoints]
+        xAxisTickLabels = xAxisValues[::7] # only set label for every 7th value
+
+        plt.grid(True)
+        plt.plot(xAxisValues, yAxisValues, color='green', marker='.')
+        plt.xlabel('Date')
+        plt.ylabel('Average Tone')
+        plt.xticks(rotation = 90)
+        axes = plt.gca()
+        axes.get_xaxis().set_ticks(xAxisTickLabels)
+        plt.show()
+
+
+    #----MAIN----
+    graphData = QuerySentimentData() #TODO: convert to __init__
+    GraphDataValues(graphData)
--- gui.py
+++ gui.py
@@ -1,17 +1,47 @@
 import tkinter
 import customtkinter
 
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("green")
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("400x240")
+class Gui(customtkinter.CTk):
 
-def button_function():
-    print("button pressed")
+    WIDTH = 780
+    HEIGHT = 520
+    #CTK = customtkinter.CTk()
 
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+    def __init__(self):
+        super().__init__()
+        
+        self.title("CustomTkinter complex_example.py")
+        self.geometry(f"{Gui.WIDTH}x{Gui.HEIGHT}")
 
-app.mainloop()
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(master=self, width=180, corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+        self.frame_right = customtkinter.CTkFrame(master=self)
+        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+
+        #----Text ("Entry") Input
+        self.entry = customtkinter.CTkEntry(
+            master=self.frame_right,
+            width=120,
+            placeholder_text="Enter Stock Ticker Symbol")
+        self.entry.grid(row=1, column=0, columnspan=2, pady=20, padx=20, sticky="we")
+
+        #----Button----
+        self.button_1 = customtkinter.CTkButton(
+            master=self.frame_right,
+            text="Search",
+            command=self.button_event)
+        self.button_1.grid(row=2, column=0, pady=10, padx=20)
+
+    def button_event(self):
+        print("Searching...")
+
+if __name__ == "__main__":
+    app = Gui()
+    app.mainloop()
